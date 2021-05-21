622ab8f83481142fd9ca022e35914a0aafcb832e
Vincent Tran <vincentuong@yahoo.com>
2021-05-21 00:18:20 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -8,14 +8,5 @@ def my_collect(array)
   collection
 end
 
-my_collect(["Tim Jones", "Tom Smith", "Jim Campagno"]) { |name| name.split(" "). first }
-def my_collect(array)
-  i = 0
-  collection=[]
-  while i < array.length
-  collection << yield(array[i])
-    i += 1
-  end
-  collection
-end
+
 
