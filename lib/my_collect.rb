0e09b74d5dd07e2b5ea221e2599b59dc346cd302
Vincent Tran <vincentuong@yahoo.com>
2021-05-21 00:12:02 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,5 +1,20 @@
-collection = []
-
-my_collect(collection)
-
+def my_collect(array)
+  i = 0
+  collection=[]
+  while i < array.length
+  collection << yield(array[i])
+    i += 1
+  end
+  collection
 end
+my_collect(["Tim Jones", "Tom Smith", "Jim Campagno"]) { |name| name.split(" "). first }
+def my_collect(array)
+  i = 0
+  collection=[]
+  while i < array.length
+  collection << yield(array[i])
+    i += 1
+  end
+  collection
+end
+my_collect(["ruby", "javascript", "python"," objective-c"]) { |lang| lang.upcase}
